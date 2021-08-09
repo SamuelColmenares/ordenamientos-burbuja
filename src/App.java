9cdb9ea0ab57ddce0635eff44bb2e64a1f0acc9e
Samuel Colmenares <locoucla@hotmail.com>
2021-08-08 22:13:42 -0500

    Descomentado ejecuciones

--- src/App.java
+++ src/App.java
@@ -3,28 +3,28 @@ public class App {
         MetodosBurbuja ordenar = new MetodosBurbuja();
 
         int arreglo[] = { 21, 40, 4, 9, 10, 35 };
-        //int arreglo2[] = { 21, 40, 4, 9, 10, 35 };
-        //int arreglo3[] = { 21, 40, 4, 9, 10, 35 };
-        //int arreglo4[] = { 21, 40, 4, 9, 10, 35 };
+        int arreglo2[] = { 21, 40, 4, 9, 10, 35 };
+        int arreglo3[] = { 21, 40, 4, 9, 10, 35 };
+        int arreglo4[] = { 21, 40, 4, 9, 10, 35 };
 
         System.out.println("METODO SIMPLE:");
         ordenar.imprimirArreglo(arreglo);
         ordenar.burbujaSimple(arreglo);
 
-        //System.out.println("________________________________________________");
-        //System.out.println("METODO MEJORADA:");
-        //ordenar.imprimirArreglo(arreglo2);
-        //ordenar.burbujaMejorada(arreglo2);
+        System.out.println("________________________________________________");
+        System.out.println("METODO MEJORADA:");
+        ordenar.imprimirArreglo(arreglo2);
+        ordenar.burbujaMejorada(arreglo2);
 
-        //System.out.println("________________________________________________");
-        //System.out.println("METODO OPTIMIZADA:");
-        //ordenar.imprimirArreglo(arreglo3);
-        //ordenar.burbujaOptimizada(arreglo3);
+        System.out.println("________________________________________________");
+        System.out.println("METODO OPTIMIZADA:");
+        ordenar.imprimirArreglo(arreglo3);
+        ordenar.burbujaOptimizada(arreglo3);
 
-        //System.out.println("________________________________________________");
-        //System.out.println("METODO FLAG:");
-        //ordenar.imprimirArreglo(arreglo4);
-        //ordenar.burbujaFlag(arreglo4);
+        System.out.println("________________________________________________");
+        System.out.println("METODO FLAG:");
+        ordenar.imprimirArreglo(arreglo4);
+        ordenar.burbujaFlag(arreglo4);
 
     }
 }
